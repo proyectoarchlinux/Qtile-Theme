3203957b712cfe6c57894c2929c8f67eb1151a7f
Daniel Nogales Iglesias <44336888+proyectoarchlinux@users.noreply.github.com>
2020-06-30 18:21:06 +0200

    Delete pantalla.sh

--- THEME QTILE/pantalla.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xrandr --newmode "1920x1080_144.00"  452.50  1920 2088 2296 2672  1080 1083 1088 1177 -hsync +vsync && xrandr --addmode DisplayPort-0 1920x1080_144.00 && xrandr --output DisplayPort-0 --mode 1920x1080_144.00
